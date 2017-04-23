67f2940e8c9b27f41e713a8479278358d86be5ac
fangxing1996 <1633617649@qq.com>
2017-04-23 19:23:56 +0800

    Create 1006.c

---
+++ 1006.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int a,s[3]={0},i;
+	scanf("%d",&a);
+	if((a>0&&a<10))
+	{
+		for(i=1;i<a+1;i++)
+			printf("%d",i);
+	}
+	else if(a<100)
+	{
+		for(i=1;i<(a/10+1);i++)
+			putchar('S');
+		for(i=1;i<(a%10+1);i++)
+			printf("%d",i);
+	}
+	else if(a<1000)
+	{
+		for(i=1;i<(a/100+1);i++)
+			putchar('B');
+		for(i=1;i<((a/10)%10+1);i++)
+			putchar('S');
+		for(i=1;i<(a%10+1);i++)
+			printf("%d",i);
+	}
+	return 0;
+}
