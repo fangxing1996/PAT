ec7b292906adacfb29ccc06314f70fa0636f221e
fangxing1996 <1633617649@qq.com>
2017-04-23 19:01:11 +0800

    Create 1057.c

---
+++ 1057.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int N=0,n0=0,n1=0;
+	char c;
+	while((c=getchar())!='\n')
+	{
+		if(c>='a'&&c<='z')
+			N=N+c-'a'+1;
+		else if(c>='A'&&c<='Z')
+		{
+			N=N+c-'A'+1;
+		}
+	}
+	if(N==0)
+	{
+		printf("0 0");
+	}
+	else
+	{
+		while(N!=0)
+		{
+			if(N%2==0)
+				n0++;
+			else
+				n1++;
+			N=N/2;
+		}
+		printf("%ld %ld",n0,n1);
+	}
+	return 0;		
+}
