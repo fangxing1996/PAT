c8104f75e6f124e43d6ba19beea3acff89c698cd
fangxing1996 <1633617649@qq.com>
2017-04-23 19:04:42 +0800

    Create 1021.c

---
+++ 1021.c
@@ -0,0 +1,22 @@
+//个位数统计 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int shu[10]={0},len,i;
+	char ch[1000];
+	scanf("%s",ch);
+	len=strlen(ch);
+	for(i=0;i<len;i++)
+	{
+		shu[ch[i]-'0']++;
+	}
+	for(i=0;i<10;i++)
+	{
+		if(shu[i]!=0)
+			printf("%d:%d\n",i,shu[i]);
+		else
+			;
+	}
+	return 0;
+} 
