0856d28b658434bb6806e544892449dbd164db2d
fangxing1996 <1633617649@qq.com>
2017-04-23 19:21:15 +0800

    Create 1046.c

---
+++ 1046.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a1,a2,b1,b2,temp,i,ta=0,tb=0;
+	scanf("%d",&temp);
+	for(i=0;i<temp;i++)
+	{
+		scanf("%d%d%d%d",&a1,&a2,&b1,&b2);
+		if(a2==(a1+b1)&&b2!=(a1+b1))
+			tb++;
+		else if(a2!=(a1+b1)&&b2==(a1+b1))
+			ta++;
+		else
+			;
+	}
+	printf("%d %d",ta,tb);
+	return 0;
+	
+}
