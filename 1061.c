176d883ae3894d14118bd0ec314d787965747f0b
fangxing1996 <1633617649@qq.com>
2017-04-28 23:45:39 +0800

    Create 1061.c

---
+++ 1061.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n,m,i,j;
+	scanf("%d",&n);
+	scanf("%d",&m);
+	int score[m]={0},answer[m]={0},in[n][m]={0},out[n]={0};
+	for(i=0;i<m;i++)
+		scanf("%d",&score[i]);
+	for(i=0;i<m;i++)
+		scanf("%d",&answer[i]);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<m;j++)
+		{
+			scanf("%d",&in[i][j]);
+			if(in[i][j]==answer[j])
+				out[i]=out[i]+score[j];
+		}
+	}
+	for(i=0;i<n;i++)
+		printf("%d\n",out[i]);
+	return 0;
+}
