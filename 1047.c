2eb53eb83031ea9682837466a0f90fc0d6a053df
方行 <fangxing1996@126.com>
2017-11-06 14:57:14 +0800

    Create 1047.c

---
+++ 1047.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,mark[1001]={0};
+	scanf("%d",&n);
+	int in[n][3];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d-%d %d",&in[i][0],&in[i][1],&in[i][2]);
+		mark[in[i][0]]+=in[i][2];
+	}
+	int nu=1,max=mark[1];
+	for(i=2;i<1001;i++)
+	{
+		if(mark[i]>max)
+		{
+			max=mark[i];
+			nu=i;
+		}
+	}
+	printf("%d %d",nu,max);
+	return 0;
+}
