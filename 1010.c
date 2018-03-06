fb9059daa50752a1f9624a431273b4044d3f8967
方行 <fangxing1996@126.com>
2018-03-06 11:37:48 +0800

    Create 1010.c

---
+++ 1010.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	char ch='a';
+	int a,b,i=0;
+	while(ch!='\n'){
+		scanf("%d %d",&a,&b);
+		if(i==0)
+		{
+			if(b==0)
+				printf("0 0");
+			else
+				printf("%d %d",a*b,b-1); 
+		}
+		else{
+			if(b!=0)
+				printf(" %d %d",a*b,b-1);
+		}
+		i++;
+		ch=getchar();
+	}
+	return 0;
+}
