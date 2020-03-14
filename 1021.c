3bc7fd24f31308ee4778735e9c2a28b47c2bcda3
方行 <fangxing1996@126.com>
2020-03-14 13:22:23 +0800

    Update 1021.c

--- 1021.c
+++ 1021.c
@@ -3,18 +3,18 @@
 #include<string.h>
 int main()
 {
-	int shu[10]={0},len,i;
+	int shu[10] = {0}, len, i;
 	char ch[1000];
-	scanf("%s",ch);
-	len=strlen(ch);
-	for(i=0;i<len;i++)
+	scanf("%s", ch);
+	len = strlen(ch);
+	for(i = 0; i < len; i++)
 	{
-		shu[ch[i]-'0']++;
+		shu[ch[i] - '0']++;
 	}
-	for(i=0;i<10;i++)
+	for(i = 0; i < 10; i++)
 	{
-		if(shu[i]!=0)
-			printf("%d:%d\n",i,shu[i]);
+		if(shu[i] != 0)
+			printf("%d:%d\n", i, shu[i]);
 		else
 			;
 	}
