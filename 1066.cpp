e710bb765fdaf5ca789ffca46fa5799a8b8ed87f
方行 <fangxing1996@126.com>
2020-03-14 15:05:41 +0800

    Update 1066.cpp

--- 1066.cpp
+++ 1066.cpp
@@ -1,36 +1,36 @@
 #include<stdio.h>
 int stdo(int n)
 {
-	if(n>=0&&n<=9)
-		printf("00%d",n);
-	else if(n>=10&&n<=99)
-		printf("0%d",n);
+	if(n >= 0 && n <= 9)
+		printf("00%d", n);
+	else if(n >= 10 && n <= 99)
+		printf("0%d", n);
 	else
-		printf("%d",n);
+		printf("%d", n);
 	return 0;
 }
 int main(){
-	int m,n,a,b,x;
-	scanf("%d %d %d %d %d",&m,&n,&a,&b,&x);
-	int i,j,in[m][n];
-	for(i=0;i<m;i++)
+	int m, n, a, b, x;
+	scanf("%d %d %d %d %d", &m, &n, &a, &b, &x);
+	int i, j, in[m][n];
+	for(i = 0; i < m; i++)
 	{
-		for(j=0;j<n;j++)
+		for(j = 0; j < n; j++)
 		{
-			scanf("%d",&in[i][j]);
-			if(in[i][j]>=a&&in[i][j]<=b)
+			scanf("%d", &in[i][j]);
+			if(in[i][j] >= a && in[i][j] <= b)
 			{
-				in[i][j]=x;
+				in[i][j] = x;
 			}
 		}
 	}
-	for(i=0;i<m;i++)
+	for(i = 0; i < m; i++)
 	{
-		for(j=0;j<n;j++)
-		{
-			if(j==0)
+		for(j = 0; j < n; j++)
+		{ 
+			if(j == 0)
 				stdo(in[i][j]);
-			else if(j==n-1)
+			else if(j == n - 1)
 			{
 				printf(" ");
 				stdo(in[i][j]);
