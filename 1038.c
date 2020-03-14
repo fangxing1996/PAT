d7f7ad94a2cd6078c724c0219281675e8519d9b5
方行 <fangxing1996@126.com>
2020-03-14 14:03:18 +0800

    Update 1038.c

--- 1038.c
+++ 1038.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
-int count[101]={0};
+int count[101] = {0};
 int main()
 {
-	int n,i,s;
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
+	int n, i, s;
+	scanf("%d", &n);
+	for(i = 0; i < n; i++)
 	{
-		scanf("%d",&s);
+		scanf("%d", &s);
 		count[s]++;
 	}
 	int m;
-	scanf("%d",&m);
-	for(i=0;i<m-1;i++)
+	scanf("%d", &m);
+	for(i = 0; i < m - 1; i++)
 	{
-		scanf("%d",&s);
-		printf("%d ",count[s]);
+		scanf("%d", &s);
+		printf("%d ", count[s]);
 	}
-	scanf("%d",&s);
-	printf("%d",count[s]);
+	scanf("%d", &s);
+	printf("%d", count[s]);
 	return 0;
 }
