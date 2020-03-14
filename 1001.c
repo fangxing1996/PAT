7afb5e36ad9360578148ea1d2acf48359ffeaa70
方行 <fangxing1996@126.com>
2020-03-14 11:25:31 +0800

    Update 1001.c

--- 1001.c
+++ 1001.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 int main()
 {
-	int n,s=0;
-	scanf("%d",&n);
-	if(n>0&&n<=1000)
+	int n, s = 0;
+	scanf("%d", &n);
+	if(n > 0 && n <= 1000)
 	{
-		while(n!=1)
+		while(n != 1)
 		{
 			if(n%2)
 			{
-				n=(3*n+1)/2;
+				n = (3*n + 1)/2;
 				s++;
 			}
 			else
 			{
-				n=n/2;
+				n = n/2;
 				s++;
 			}
 		}
 	}
 	else
 		printf("error");
-	printf("%d",s);
+	printf("%d", s);
 	return 0;
 }
