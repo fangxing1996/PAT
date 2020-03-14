60cee4b6326c409b3f89243816bf9040580cec31
方行 <fangxing1996@126.com>
2020-03-14 13:24:02 +0800

    Update 1023.c

--- 1023.c
+++ 1023.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 int main()
 {
-	int in[10],i,j,a;
-	for(i=0;i<10;i++)
+	int in[10], i, j, a;
+	for(i = 0; i < 10; i++)
 	{
-		scanf("%d",&in[i]);
+		scanf("%d", &in[i]);
 	}
-	i=1;
-	while(in[i++]==0);
-	a=i-1;
-	printf("%d",a);
-	for(j=0;j<10;j++)
+	i = 1;
+	while(in[i++] == 0);
+	a = i - 1;
+	printf("%d", a);
+	for(j = 0; j < 10; j++)
 	{
-		if(j!=a)
+		if(j != a)
 		{
-			for(i=0;i<in[j];i++)
-				printf("%d",j);
+			for(i = 0; i < in[j]; i++)
+				printf("%d", j);
 		}
 		else
 		{
-			for(i=0;i<in[a]-1;i++)
-				printf("%d",a);
+			for(i = 0; i < in[a] - 1; i++)
+				printf("%d", a);
 		}
 	}
 	return 0;
