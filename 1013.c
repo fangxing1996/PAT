ba6d9f533bfe6440585a8e3b119f631047b93e6e
方行 <fangxing1996@126.com>
2020-03-14 12:49:58 +0800

    Update 1013.c

--- 1013.c
+++ 1013.c
@@ -3,24 +3,24 @@
 int isprime(int n)
 {
 	int i;
-	if(n==1)
+	if(n == 1)
 		return 0;
-	else if(n==2||n==3||n==5||n==7)
+	else if(n == 2 || n == 3 || n == 5 || n == 7)
 		return 1;
 	else
 	{
-		if((n%2==0)||(n%3==0)||(n%5==0)||(n%7==0))
+		if((n%2 == 0) || (n%3 == 0) || (n%5 == 0) || (n%7 == 0))
 			return 0;
 		else
 		{
-			for(i=2;i<=sqrt(n);i++)
+			for(i = 2; i <= sqrt(n); i++)
 			{
 				if(!(n%i)) 
 				{
 					return 0; 
 					break; 
 				}
-				else if(i+1>(sqrt(n)+1))
+				else if(i + 1 > (sqrt(n) + 1))
 					return 1;
 			}
 		}
@@ -28,31 +28,31 @@ int isprime(int n)
 }
 int main()
 {
-	int m,n,count=0,i=2,prime[10000];
-	scanf("%d%d",&m,&n);
-	while(count<n)
+	int m, n, count = 0, i = 2, prime[10000];
+	scanf("%d%d", &m, &n);
+	while(count < n)
 	{
 		if(isprime(i))
 		{
-			prime[count]=i;
+			prime[count] = i;
 			count++;
 		}
 		i++;
 	}
-	count=0;
-	for(i=m-1;i<n-1;i++)
+	count = 0;
+	for(i = m - 1; i < n - 1; i++)
 	{
-		if(count==9)
+		if(count == 9)
 		{
-			printf("%ld\n",prime[i]);
-			count=0;
+			printf("%ld\n", prime[i]);
+			count = 0;
 		}
 		else
 		{
-			printf("%ld ",prime[i]);
+			printf("%ld ", prime[i]);
 			count++;
 		}
 	}
-	printf("%ld",prime[n-1]);
+	printf("%ld", prime[n - 1]);
 	return 0;
 }
