dff6fdde875c254bee94c3897960f398c07b4e4e
方行 <fangxing1996@126.com>
2020-03-14 13:42:33 +0800

    Update 1029.c

--- 1029.c
+++ 1029.c
@@ -1,38 +1,38 @@
 #include<stdio.h>
-int flag(char *a,char b,int x){
+int flag(char *a, char b, int x){
 	int j;
-	for(j=0;j<x;j++){
-		if(a[j]==b)
+	for(j = 0; j < x; j++){
+		if(a[j] == b)
 			return 1;
 	}
 	return 0;
 }
 int main(){
-	char ch[81],r[81],badkey[81],c;
-	int i=0,j=0,count=0;
-	while((c=getchar())!='\n'){
-		ch[i++]=c;
+	char ch[81], r[81], badkey[81], c;
+	int i = 0, j = 0, count = 0;
+	while((c = getchar()) != '\n'){
+		ch[i++] = c;
 	}
-	ch[i++]='\0';
-	while((c=getchar())!='\n'){
-		r[j++]=c;
+	ch[i++] = '\0';
+	while((c = getchar()) != '\n'){
+		r[j++] = c;
 	}
-	r[j++]='\0';
-	char *m=ch,*n=r;
-	while(*m!='\0'&&n!='\0'){
-		if(*m!=*n){
-			if(*m<='z'&&*m>='a'){
-				if(flag(badkey,*m-'a'+'A',count))
+	r[j++] = '\0';
+	char *m = ch, *n = r;
+	while(*m != '\0 '&& n != '\0'){
+		if(*m != *n){
+			if(*m <= 'z' && *m >= 'a'){
+				if(flag(badkey, *m - 'a' + 'A', count))
 					;
 				else{
-					badkey[count++]=*m-'a'+'A';
+					badkey[count++] = *m - 'a' + 'A';
 				}
 			}
 			else{
-				if(flag(badkey,*m,count))
+				if(flag(badkey, *m, count))
 					;
 				else{
-					badkey[count++]=*m;
+					badkey[count++] = *m;
 				}
 			}
 			m++;
@@ -42,7 +42,7 @@ int main(){
 			n++;
 		}
 	}
-	badkey[count]='\0';
-	printf("%s",badkey);
+	badkey[count] = '\0';
+	printf("%s", badkey);
 	return 0;
 }
