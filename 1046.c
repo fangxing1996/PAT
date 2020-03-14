8b1543ee4963107b5d683d671906991cf1a90b75
方行 <fangxing1996@126.com>
2020-03-14 14:39:54 +0800

    Update 1046.c

--- 1046.c
+++ 1046.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main()
 {
-	int a1,a2,b1,b2,temp,i,ta=0,tb=0;
-	scanf("%d",&temp);
-	for(i=0;i<temp;i++)
+	int a1, a2, b1, b2, temp, i, ta = 0, tb = 0;
+	scanf("%d", &temp);
+	for(i = 0; i < temp; i++)
 	{
-		scanf("%d%d%d%d",&a1,&a2,&b1,&b2);
-		if(a2==(a1+b1)&&b2!=(a1+b1))
+		scanf("%d%d%d%d", &a1, &a2, &b1, &b2);
+		if(a2 == (a1 + b1) && b2 != (a1 + b1))
 			tb++;
-		else if(a2!=(a1+b1)&&b2==(a1+b1))
+		else if(a2 != (a1 + b1) && b2 == (a1 + b1))
 			ta++;
 		else
 			;
 	}
-	printf("%d %d",ta,tb);
+	printf("%d %d", ta, tb);
 	return 0;
 	
 }
