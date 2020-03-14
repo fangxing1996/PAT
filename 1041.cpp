8e2a01b11acfc19feda85a3e8fd3594cacf8c9e5
方行 <fangxing1996@126.com>
2020-03-14 14:38:08 +0800

    Update 1041.cpp

--- 1041.cpp
+++ 1041.cpp
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main(){
-	int n,m,i,j,k=0,temp;
-	scanf("%d",&n);
-	char ch[n][15],s[15]={'\0'},r[15]={'\0'};
-	int a[n],b[n];
-	for(i=0;i<n;i++)
+	int n, m, i, j, k = 0, temp;
+	scanf("%d", &n);
+	char ch[n][15], s[15] = {'\0'}, r[15] = {'\0'};
+	int a[n], b[n];
+	for(i = 0; i < n; i++)
 	{
-		scanf("%s %d %d",ch[i],&a[i],&b[i]);
+		scanf("%s %d %d", ch[i], &a[i], &b[i]);
 	}
-	scanf("%d",&m);
-	while(k<m)
-	{	scanf("%d",&temp);
-		for(i=0;i<n;i++)
+	scanf("%d", &m);
+	while(k < m)
+	{	scanf("%d", &temp);
+		for(i = 0; i < n; i++)
 		{
-			if(a[i]==temp)
-				printf("%s %d\n",ch[i],b[i]);
+			if(a[i] == temp)
+				printf("%s %d\n", ch[i], b[i]);
 		}
 		k++;
 	}
