4d80e47107de3bae9652fc3258794c0f33e2a339
方行 <fangxing1996@126.com>
2020-03-14 11:30:10 +0800

    Update 1002.c

--- 1002.c
+++ 1002.c
@@ -1,56 +1,56 @@
 #include<stdio.h>
 #include<string.h>
 char ch[15];
-int i=0;
+int i = 0;
 /*整数拼音输出*/
 int pinyin(int n)
 {
 	switch(n)
 	{
-		case 9:ch[i++]='j';
-				ch[i++]='i';
-				ch[i++]='u';
-				ch[i++]=' ';
+		case 9:ch[i++] = 'j';
+				ch[i++] = 'i';
+				ch[i++] = 'u';
+				ch[i++] = ' ';
 				break;
-		case 8:ch[i++]='b';
-				ch[i++]='a';
-				ch[i++]=' ';
+		case 8:ch[i++] = 'b';
+				ch[i++] = 'a';
+				ch[i++] = ' ';
 				break;
-		case 7:ch[i++]='q';
-				ch[i++]='i';
-				ch[i++]=' ';
+		case 7:ch[i++] = 'q';
+				ch[i++] = 'i';
+				ch[i++] = ' ';
 				break;
-		case 6:ch[i++]='l';
-				ch[i++]='i';
-				ch[i++]='u';
-				ch[i++]=' ';
+		case 6:ch[i++] = 'l';
+				ch[i++] = 'i';
+				ch[i++] = 'u';
+				ch[i++] = ' ';
 				break;
-		case 5:ch[i++]='w';
-				ch[i++]='u';
-				ch[i++]=' ';
+		case 5:ch[i++] = 'w';
+				ch[i++] = 'u';
+				ch[i++] = ' ';
 				break;
-		case 4:ch[i++]='s';
-				ch[i++]='i';
-				ch[i++]=' ';
+		case 4:ch[i++] = 's';
+				ch[i++] = 'i';
+				ch[i++] = ' ';
 				break;
-		case 3:ch[i++]='s';
-				ch[i++]='a';
-				ch[i++]='n';
-				ch[i++]=' ';
+		case 3:ch[i++] = 's';
+				ch[i++] = 'a';
+				ch[i++] = 'n';
+				ch[i++] = ' ';
 				break;
-		case 2:ch[i++]='e';
-				ch[i++]='r';
-				ch[i++]=' ';
+		case 2:ch[i++] = 'e';
+				ch[i++] = 'r';
+				ch[i++] = ' ';
 				break;
-		case 1:ch[i++]='y';
-				ch[i++]='i';
-				ch[i++]=' ';
+		case 1:ch[i++] = 'y';
+				ch[i++] = 'i';
+				ch[i++] = ' ';
 				break;
-		case 0:ch[i++]='l';
-				ch[i++]='i';
-				ch[i++]='n';
-				ch[i++]='g';
-				ch[i++]=' ';
+		case 0:ch[i++] = 'l';
+				ch[i++] = 'i';
+				ch[i++] = 'n';
+				ch[i++] = 'g';
+				ch[i++] = ' ';
 				break; 
 	}
 	return 0;
@@ -68,16 +68,16 @@ int printd(int n)
 }
 int main()
 {
-	int n=0,len;
+	int n = 0, len;
 	char in[100];
-	scanf("%s",in);
-	len=strlen(in);
-	for(i=0;i<len;i++)
-		n=n+in[i]-'0';
-	i=0;
+	scanf("%s", in);
+	len = strlen(in);
+	for(i = 0; i < len; i++)
+		n = n + in[i] - '0';
+	i = 0;
 	printd(n);
 	i--;
-	ch[i]='\0';
-	printf("%s",ch);
+	ch[i] = '\0';
+	printf("%s", ch);
 	return 0;
 }
