b94f36cc4790107ce23c2557e154fd1feddb7c94
方行 <fangxing1996@126.com>
2020-03-14 11:33:25 +0800

    Update 1003.c

--- 1003.c
+++ 1003.c
@@ -1,43 +1,43 @@
 #include<stdio.h>
-int check(char *t)
+int check(char* t)
 {
-	char *p=t;
-	int f1=0,f2=0,f3=0,fp=0,ft=0;
-	while(*p!='\0')
+	char* p = t;
+	int f1 = 0, f2 = 0, f3 = 0, fp = 0, ft = 0;
+	while(*p != '\0')
 	{
-		if((*p!='P')&&(*p!='A')&&(*p!='T'))
+		if((*p != 'P')&&(*p != 'A')&&(*p != 'T'))
 		{
 			return 0;
 		}
-		else if(*p=='T'&&fp==0)
+		else if(*p == 'T' && fp == 0)
 		{
 			return 0;
 		}
-		else if(*p=='P'&&fp==1){
+		else if(*p == 'P' && fp == 1){
 			return 0;
 		}
-		else if(*p=='T'&&ft==1){
+		else if(*p == 'T' && ft == 1){
 			return 0;
 		}
-		else if(*p=='P'){
-			fp=1;
+		else if(*p == 'P'){
+			fp = 1;
 		}
-		else if(*p=='T'){
-			ft=1;
+		else if(*p == 'T'){
+			ft = 1;
 		}
-		else if(*p=='A')
+		else if(*p == 'A')
 		{
-			if(fp==0)
+			if(fp == 0)
 			   f1++;
-			else if(fp==1&&ft==0)
+			else if(fp == 1 && ft == 0)
 			   f2++;
-			else if(fp==1&&ft==1)
+			else if(fp == 1 && ft == 1)
 			   f3++;
 		}
 		p++;
 	}
-	if(f2!=0&&fp==1&&ft==1){
-		if(f1*f2==f3){
+	if(f2 != 0 && fp == 1 && ft == 1){
+		if(f1*f2 == f3){
 			return 1;
 		}
 		else 
@@ -49,10 +49,10 @@ int check(char *t)
 int main()
 {
 	int n;
-	scanf("%d",&n);
+	scanf("%d", &n);
 	char ch[101];
 	while(n--)
-	{	scanf("%s",ch);
+	{	scanf("%s", ch);
 		if(check(ch)){
 			printf("YES\n");
 		}
