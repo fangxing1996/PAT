ac78ec2f3a0352585d72493114ce4a1842985d94
方行 <fangxing1996@126.com>
2020-03-14 14:07:11 +0800

    Update 1039.c

--- 1039.c
+++ 1039.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 #include<string.h>
-int bubble(char a[],int n)
+int bubble(char a[], int n)
 {
-	int i,j,temp;
-	for(i=0;i<n;i++)
+	int i, j, temp;
+	for(i = 0; i < n; i++)
 	{
-		for(j=1;j<n-i;j++)
+		for(j = 1; j < n - i; j++)
 		{
-			if(a[j]<a[j-1])
+			if(a[j] < a[j - 1])
 			{
-				temp=a[j];
-				a[j]=a[j-1];
-				a[j-1]=temp;
+				temp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = temp;
 			}	
 		}
 	}
@@ -19,32 +19,32 @@ int bubble(char a[],int n)
 }
 int main()
 {
-	char a[1001],b[1001];
-	int t=0,len_a,len_b,i,j,k;
-	scanf("%s",a);
-	scanf("%s",b);
-	len_a=strlen(a);
-	len_b=strlen(b);
-	bubble(a,len_a);
-	bubble(b,len_b);
-	k=0;
-	for(i=0;i<len_b;i++)
+	char a[1001], b[1001];
+	int t = 0, len_a, len_b, i, j, k;
+	scanf("%s", a);
+	scanf("%s", b);
+	len_a = strlen(a);
+	len_b = strlen(b);
+	bubble(a, len_a);
+	bubble(b, len_b);
+	k = 0;
+	for(i = 0; i < len_b; i++)
 	{
-		for(j=k;j<len_a;j++)
+		for(j = k; j < len_a; j++)
 		{
-			if(b[i]==a[j])
+			if(b[i] == a[j])
 			{
-				k=j+1;
+				k = j + 1;
 				t++;
 				break;
 			}
 		}
 	}
-	if(t==len_b)
+	if(t == len_b)
 	{
-		printf("Yes %d",len_a-len_b);
+		printf("Yes %d", len_a - len_b);
 	}
 	else
-		printf("No %d",len_b-t);
+		printf("No %d", len_b - t);
 	return 0;
 }
