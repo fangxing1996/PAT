677f413fc4246735b0f833ff226cbd8feada171b
方行 <fangxing1996@126.com>
2020-03-14 14:52:28 +0800

    Update 1056.c

--- 1056.c
+++ 1056.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-int bubble(int a[],int n)
+int bubble(int a[], int n)
 {
-	int i,j,temp;
-	for(i=0;i<n;i++)
+	int i, j, temp;
+	for(i = 0; i < n; i++)
 	{
-		for(j=1;j<n-i;j++)
+		for(j = 1; j < n - i; j++)
 		{
-			if(a[j]<a[j-1])
+			if(a[j] < a[j - 1])
 			{
-				temp=a[j];
-				a[j]=a[j-1];
-				a[j-1]=temp;
+				temp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = temp;
 			}
 		}
 	}
@@ -18,30 +18,30 @@ int bubble(int a[],int n)
 }
 int main()
 {
-	int n,i,j,t=-1,sum=0;
-	scanf("%d",&n);
+	int n, i, j, t = -1, sum=0;
+	scanf("%d", &n);
 	int s[n];
-	for(i=0;i<n;i++)
+	for(i = 0; i < n; i++)
 	{
-		scanf("%d",&s[i]);
+		scanf("%d", &s[i]);
 	}
-	bubble(s,n);
-	for(i=0;i<n;i++)
+	bubble(s, n);
+	for(i = 0; i < n; i++)
 	{
-		if(s[i]!=t)
+		if(s[i] != t)
 		{
-			for(j=i+1;j<n;j++)
+			for(j = i + 1; j < n; j++)
 			{
-				if(s[i]==s[j])
+				if(s[i] == s[j])
 				{	
-					sum+=s[i]*10+s[i];
-					t=j;
+					sum += s[i]*10 + s[i];
+					t = j;
 				}
 				else
-					sum+=(s[i]+s[j])*11;
+					sum += (s[i] + s[j])*11;
 			}
 		}
 	}
-	printf("%d",sum);
+	printf("%d", sum);
 	return 0;
 }
