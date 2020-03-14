95b8320111ee6154f20b5f02cb4fd772959a8c57
方行 <fangxing1996@126.com>
2020-03-14 11:45:36 +0800

    Update 1006.c

--- 1006.c
+++ 1006.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
 int main()
 {
-	int a,s[3]={0},i;
-	scanf("%d",&a);
-	if((a>0&&a<10))
+	int a, s[3] = {0}, i;
+	scanf("%d", &a);
+	if((a > 0 && a < 10))
 	{
-		for(i=1;i<a+1;i++)
-			printf("%d",i);
+		for(i = 1; i < a + 1; i++)
+			printf("%d", i);
 	}
-	else if(a<100)
+	else if(a < 100)
 	{
-		for(i=1;i<(a/10+1);i++)
+		for(i = 1; i < (a/10 + 1); i++)
 			putchar('S');
-		for(i=1;i<(a%10+1);i++)
-			printf("%d",i);
+		for(i = 1; i < (a%10 + 1); i++)
+			printf("%d", i);
 	}
-	else if(a<1000)
+	else if(a < 1000)
 	{
-		for(i=1;i<(a/100+1);i++)
+		for(i = 1; i < (a/100 + 1); i++)
 			putchar('B');
-		for(i=1;i<((a/10)%10+1);i++)
+		for(i = 1; i < ((a/10)%10 + 1); i++)
 			putchar('S');
-		for(i=1;i<(a%10+1);i++)
-			printf("%d",i);
+		for(i = 1; i < (a%10 + 1); i++)
+			printf("%d", i);
 	}
 	return 0;
 }
