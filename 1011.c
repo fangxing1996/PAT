ddfc0bef39e0b210512786be97b00fdaaf85be27
方行 <fangxing1996@126.com>
2020-03-14 12:43:17 +0800

    Update 1011.c

--- 1011.c
+++ 1011.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main()
 {
-	int x,i,p;
-	scanf("%d",&x);
-	long long int in[x][3],t;
-	for(i=0;i<x;i++)
+	int x, i, p;
+	scanf("%d", &x);
+	long long int in[x][3], t;
+	for(i = 0; i < x; i++)
 	{
-		scanf("%lld %lld %lld",&in[i][0],&in[i][1],&in[i][2]);
+		scanf("%lld %lld %lld", &in[i][0], &in[i][1], &in[i][2]);
 	}
-	for(i=0;i<x;i++)
+	for(i = 0; i < x; i++)
 	{
-		p=i+1;
-		t=in[i][0]+in[i][1];
-		if(t>in[i][2])
-			printf("Case #%d: true\n",p);
+		p = i + 1;
+		t = in[i][0] + in[i][1];
+		if(t > in[i][2])
+			printf("Case #%d: true\n", p);
 		else
-			printf("Case #%d: false\n",p);
+			printf("Case #%d: false\n", p);
 	}
 	return 0;
 }
